9a58186579774a2186a4909fa0e0b4efece4c678
MaikHilton <150069012+MaikHilton@users.noreply.github.com>
2024-08-20 18:07:15 +0300

    Лабораторна робота 2(Python) Завдання 2.py

--- Лабораторна робота 2(Python) Завдання 2.py
+++ Лабораторна робота 2(Python) Завдання 2.py
@@ -1,15 +1,4 @@
-import math
-
 from lab2_module import def_number  # Імпорт функції з модуля
-
-# Функція для обчислення значення виразу z
-def calculate_z(x):
-    return (math.sqrt(2) / 2) * math.sin(1 / x) + 1
-
-# Основна програма
-x = float(input("Введіть значення x: "))
-z = calculate_z(x)
-print(f"Значення z: {z}")
 
 n = int(input("Введіть число n: "))
 if def_number(n):
